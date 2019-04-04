f4b3de7c8a0c5dd31469e0fe9a1169e46563670f
Paweł Gliwny <p.gliwny@gmail.com>
2019-04-04 22:18:13 +0200

    clean notebook

--- time_corr/time_cal_corr.py
+++ time_corr/time_cal_corr.py
@@ -1,22 +1,27 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 class TimeCalCorr:
     def __init__(self, n_combine, n_harm, n_cap):
-        self.fMeanVal = np.zeros(128)
-        self.fNumMean = np.zeros(128)
+        n = int(n_cap/n_combine)
+        self.fMeanVal = np.zeros(n)
+        self.fNumMean = np.zeros(n)
+        self.fRms = np.zeros(n)
         self.fNumCombine = n_combine
         self.fNumHarmonics = n_harm
         self.fNumCap = n_cap
-        self.fNumPoints = int((self.fNumCap ) /(self.fNumCombine))
+        self.fNumPoints = int(self.fNumCap/self.fNumCombine)
 
     def fill(self, first_cap, arrslice):
         fBin = int(first_cap /self.fNumCombine)
         self.fMeanVal[fBin] += arrslice
         self.fNumMean[fBin] += 1
+        self.fRms[fBin] += arrslice * arrslice
 
     def finalize(self):
         self.fMeanVal = self.fMeanVal /self.fNumMean
+        self.fRms = np.sqrt((self.fRms / self.fNumMean) - self.fMeanVal ** 2)
 
         self.pos = np.zeros(self.fNumPoints)
         for i in range(0, self.fNumPoints):
@@ -45,3 +50,37 @@ class TimeCalCorr:
             time += self.fan[n] * np.cos((first_cap * n * 2 * np.pi) / self.fNumCap)
             time += self.fbn[n] * np.sin((first_cap * n * 2 * np.pi) / self.fNumCap)
         return time
+
+
+def plot_corr_curve(n, n_cap, n_combine, an, bn, fMeanVal):
+    fc = np.arange(0, n_cap, n_combine)
+    y = np.zeros(n)
+
+    for i in range(0, len(y)):
+        temp_cos = an[0] / 2
+        temp_sin = 0
+        for j in range(1, len(an)):
+            temp_cos += an[j] * np.cos(2 * j * np.pi * (fc[i] / 1024.))
+            temp_sin += bn[j] * np.sin(2 * j * np.pi * (fc[i] / 1024.))
+        y[i] = (temp_cos + temp_sin)
+
+    fig, ax = plt.subplots(figsize=(16, 9))
+    ax.plot(np.arange(0, n_cap, n_combine), fMeanVal, 'bo')
+    ax.plot(fc, y, 'r--')
+    ax.set_ylabel("Mean arrival time")
+    ax.set_xlabel("Position in the DRS ring")
+
+
+def make_hist(arrival_time_list, arrival_time_corr_list, binwidth = 0.5):
+    plt.figure(figsize=(16, 9))
+    plt.hist(arrival_time_list, histtype='step', lw=3, linestyle='--', range=(20, 30),
+             bins=np.arange(min(arrival_time_list), max(arrival_time_list) + binwidth, binwidth),
+             label="before time corr")
+
+    plt.hist(arrival_time_corr_list, histtype='step', lw=3, linestyle='--', color='red', range=(20, 30),
+             bins=np.arange(min(arrival_time_corr_list), max(arrival_time_corr_list) + binwidth, binwidth),
+             label="after time corr")
+    plt.ylabel("Number of events")
+    plt.xlabel("Arrival time [time samples 1 ns]")
+    plt.legend()
+    plt.tight_layout()
